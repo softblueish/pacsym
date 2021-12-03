acac014972321140bd3529396f3e68b34ed5af5a
Alex S <75302078+InfinityFloof@users.noreply.github.com>
2021-12-03 12:14:11 +0100

    New features

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 pacsym:
   go build pacsym.go
 install:
-  mkdir --parents /usr/pkg/pacsym/1.0/bin/
-  mv pacsym /usr/pkg/pacsym/1.0/bin/pacsym
+  mkdir --parents /usr/pkg/pacsym/2.0/bin/
+  mv pacsym /usr/pkg/pacsym/2.0/bin/pacsym
+  mv pacsym /usr/pkgsrc/
--- pacsym.go
+++ pacsym.go
@@ -11,14 +11,27 @@ import (
 	"strings"
 )
 
+// Arguments
+var pacsumArguments = []string{"-s", "--separate-builddir", "--local", "-l"}
+
+// Variables for sync
 var (
 	packagePath  = []string{}
 	filePath     = []string{}
 	trueFilePath = []string{}
 )
 
+// Variables for build
+var (
+	tarName        string
+	tarPath        string
+	url            string
+	buildarguments = []string{}
+	makearguments  string
+)
+
 func help() {
-	fmt.Printf("sync - Symlinks all installed packages in /usr/pkg\n")
+	fmt.Printf("sync - Symlinks all installed packages in /usr/pkg\nbuild <URL> [OPTIONS] [MAKEFLAGS] - Compiles a package from url or local file\n--local -l - Compiles from local file, takes a filepath instead of url\n--separate-builddir -s - Compiles in a separate build directory")
 }
 
 func walk(s string, d fs.DirEntry, err error) error {
@@ -29,6 +42,15 @@ func walk(s string, d fs.DirEntry, err error) error {
 		filePath = append(filePath, s)
 	}
 	return nil
+}
+
+func inList(x []string, y string) bool {
+	for i := 0; i < len(x); i++ {
+		if strings.Compare(y, x[i]) == 0 {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -45,7 +67,6 @@ func main() {
 			}
 
 			for _, f := range files {
-				fmt.Println(f.Name())
 				packagePath = append(packagePath, "/usr/pkg/"+f.Name())
 			}
 
@@ -57,7 +78,6 @@ func main() {
 				}
 
 				for _, f := range files {
-					fmt.Println(f.Name())
 					packagePath[i] = packagePath[i] + "/" + f.Name()
 
 					// Reads binary file
@@ -72,10 +92,129 @@ func main() {
 
 			// Create symlinks
 			for i := 0; i < len(filePath); i++ {
-				exec.Command("ln -svf " + filePath[i] + " " + trueFilePath[i])
-				fmt.Println("Executed: " + "ln -svf " + filePath[i] + " " + trueFilePath[i])
+				cmd := exec.Command("mkdir", "--verbose", "--parents", strings.Join(strings.Split(trueFilePath[i], "/")[:len(strings.Split(trueFilePath[i], "/"))-1], "/"))
+				stdout, err := cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
+				cmd = exec.Command("ln", "-svf", filePath[i], trueFilePath[i])
+				stdout, err = cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
 			}
 
+			fmt.Println("Done.")
+
+		// Downloads packages and compiles it with set flags
+		case "build":
+			buildarguments = os.Args[3:]
+
+			// Checks if its a local package
+			if inList(buildarguments, "--local") || inList(buildarguments, "-l") {
+
+				// Finds and indexes package
+				tarPath = os.Args[2]
+				tarName = (strings.Split(tarPath, "/")[len(strings.Split(tarPath, "/"))-1])
+			} else {
+
+				// Downloads, finds and indexes package
+				url = os.Args[2]
+				cmd := exec.Command("wget", url)
+				stdout, err := cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
+				tarName = (strings.Split(url, "/")[len(strings.Split(url, "/"))-1])
+				tarPath = "./" + tarName
+			}
+			fmt.Println(tarName)
+
+			// Moves package to /usr/pkgsrc/, aka build location.
+			cmd := exec.Command("cp", "-v", tarPath, "/usr/pkgsrc/")
+			stdout, err := cmd.Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(string(stdout))
+
+			// Extract tarball
+			cmd = exec.Command("tar", "xvf", tarName, "-C", "/usr/pkgsrc/")
+			stdout, err = cmd.Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			builddir := "/usr/pkgsrc/" + tarName[:len(tarName)-7]
+
+			if inList(buildarguments, "--separate-builddir") || inList(buildarguments, "-s") {
+				// Create separate builddir
+				builddir = "/usr/pkgsrc/" + tarName[:len(tarName)-7] + "/build"
+				cmd = exec.Command("mkdir", "-v", builddir)
+				stdout, err = cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
+			}
+
+			// Changes dir into builddir
+			os.Chdir(builddir)
+
+			// Removes pacsym specific arguments
+			for i := 0; i < len(buildarguments); i++ {
+				if inList(pacsumArguments, buildarguments[i]) == false {
+					makearguments = makearguments + " " + buildarguments[i]
+				}
+			}
+
+			fmt.Println("Configuring...")
+			if inList(buildarguments, "--separate-builddir") || inList(buildarguments, "-s") {
+				// Configures package if in builddir
+				cmd = exec.Command("../configure", makearguments)
+				stdout, err = cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
+			} else {
+				// Configures package
+				cmd = exec.Command("./configure", makearguments)
+				stdout, err = cmd.Output()
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(string(stdout))
+			}
+
+			// Builds package
+			fmt.Println("Compiling...")
+			cmd = exec.Command("make")
+			stdout, err = cmd.Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(string(stdout))
+
+			// Removes package
+			cmd = exec.Command("rm", "-r", "/usr/pkgsrc/*")
+			stdout, err = cmd.Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(string(stdout))
 			fmt.Println("Done.")
 
 		default:
