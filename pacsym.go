3e333e2b8204f278eb5af42101ce027dfb5ed3fb
rtw <rtiannew@gmail.com>
2021-12-04 23:40:39 +0000

    Comments
    Made the comments "shorter" to accomodate people with smaller screens,
    and fixed some typos

--- pacsym.go
+++ pacsym.go
@@ -86,7 +86,8 @@ func main() {
 		// This switch statement activates depending on what they type next
 		switch os.Args[1] {
 
-		// When the user types sync, it is to find all packages in the /usr/pkg/ directory and symlink them according to its hiearchy
+		// When the user types sync, it is to find all packages in the /usr/pkg/ directory and
+		// symlinks them according to its hiearchy
 		case "sync":
 
 			// This will read all directories in /usr/pkg, which will be used later
@@ -95,12 +96,15 @@ func main() {
 				log.Fatal(err)
 			}
 
-			// This for loop will make sure the directory of every package in /usr/pkg is catalouged in the packagePath array
+			// This for loop will make sure the directory of every package in /usr/pkg is 
+			// catalogued in the packagePath array
 			for _, f := range files {
 				packagePath = append(packagePath, "/usr/pkg/"+f.Name())
 			}
 
-			// This nested for loop will look in every package with the help of the packagePath array and add the version of the package to the packagePath array
+			// This nested for loop will look in every package with the help of the packagePath
+			// array and add the version of the package to the packagePath array
+			//
 			// An example would be /usr/pkg/foobar/1.1, which would be an element of /usr/pkg
 			for i := 0; i < len(packagePath); i++ {
 				files, err := ioutil.ReadDir(packagePath[i])
@@ -111,19 +115,26 @@ func main() {
 				for _, f := range files {
 					packagePath[i] = packagePath[i] + "/" + f.Name()
 
-					// When we reach this point in the for loop, we will index every file in the directory who are elements in the packagePath array and place them in the filepath array
+					// When we reach this point in the for loop, we will index every file in the directory
+					// who are elements in the packagePath array and place them in the filepath array
 					filepath.WalkDir(packagePath[i], walk)
 				}
 			}
 
-			// The for loop described under will tear appart every element in filePath, remove /usr/pkg/<PACKAGENAME>/<PACKAGEVERSION>/ and make it an element of trueFilePath
-			// An example would be the element /usr/pkg/foobar/1.1/usr/bin/dependency.sh would turn into just /usr/bin/dependency.sh
+			// The for loop described under will tear appart every element in filePath, remove
+			// /usr/pkg/<PACKAGENAME>/<PACKAGEVERSION>/ and make it an element of trueFilePath
+			// An example would be the element /usr/pkg/foobar/1.1/usr/bin/dependency.sh would
+			// turn into just /usr/bin/dependency.sh
+			
 			for i := 0; i < len(filePath); i++ {
 				trueFilePath = append(trueFilePath, "/"+strings.Join(strings.Split(filePath[i], "/")[5:], "/"))
 			}
 
-			// Now it's time for the main attraction, linking every element in filePath to trueFilePath, this will recursively repeat for every element present in the array filePath
+			// Now it's time for the main attraction, linking every element in filePath to trueFilePath, this will
+			// recursively repeat for every element present in the array filePath
+			//
 			// An example of a symlink would be /usr/bin/dependency.sh would be symlinked to /usr/pkg/foobar/1.1/usr/bin/dependency.sh
+			
 			for i := 0; i < len(filePath); i++ {
 				cmd := exec.Command("mkdir", "--verbose", "--parents", strings.Join(strings.Split(trueFilePath[i], "/")[:len(strings.Split(trueFilePath[i], "/"))-1], "/"))
 				stdout, err := cmd.CombinedOutput()
@@ -144,23 +155,30 @@ func main() {
 			// At this point the pacsym program will end.
 			fmt.Println("Done.")
 
-		// If the "build" option was chosen, the package manager will attempt to place a tarball in /usr/pkgsrc, extract it, configure it and finally, build it.
+		// If the "build" option was chosen, the package manager will attempt to place a tarball in /usr/pkgsrc
+		// extract it, configure it and finally, build it.
 		case "build":
 
-			// When building a package, it might require certian arguments to work correctly, that means everything written after pacsym build <tarball> will be counted as a build argument, unless it's a pacsym argument
+			// When building a package, it might require certian arguments to work correctly, that means everything
+			// written after pacsym build <tarball> will be counted as a build argument, unless it's a pacsym argument
 			buildarguments = os.Args[3:]
 
-			// Here's the first usecase of the pacsym arguments, "--local" or "-l", this will check if it exists in the buildarguments array. If it is present, it will just index the tarball, if its not, it will download the url present with wget, then index it.
+			// Here's the first usecase of the pacsym arguments, "--local" or "-l", this will check if it exists in
+			// the buildarguments array. If it is present, it will just index the tarball, if its not, it will
+			// download the url present with wget, then index it.
 			if inList(buildarguments, "--local") || inList(buildarguments, "-l") {
 
-				// This will check os.Args[2], which the program assumes to be the location of the tarball, and index it to tarPath.
+				// This will check os.Args[2], which the program assumes to be the location of the tarball,
+				// and index it to tarPath.
 				tarPath = os.Args[2]
 
-				// This will take tarPath and take the last word before it finds a /, which will leave just the name of the tarball and index it to tarName.
+				// This will take tarPath and take the last word before it finds a /, which will leave just
+				// the name of the tarball and index it to tarName.
 				tarName = (strings.Split(tarPath, "/")[len(strings.Split(tarPath, "/"))-1])
 			} else {
 
-				// If the --local option wasnt specified, os.Args will assume that the thing after "build" written is the url to the tarball, then index that url to the url variable.
+				// If the --local option wasnt specified, os.Args will assume that the thing after "build" 
+				// written is the url to the tarball, then index that url to the url variable.
 				url = os.Args[2]
 
 				// After which, a wget command will be executed and download the tarball.
@@ -172,7 +190,8 @@ func main() {
 				}
 				fmt.Println(string(stdout))
 
-				// This will take the url, find the last segment of it which would be the name of the tarball and index it.
+				// This will take the url, find the last segment of it which would be the name of the
+				// tarball and index it.
 				tarName = (strings.Split(url, "/")[len(strings.Split(url, "/"))-1])
 
 				// This is self explanitory, it takes the name of the tarball and adds ./, which means current directory.
@@ -205,14 +224,16 @@ func main() {
 				return
 			}
 
-			// The directory it was extracted to will be indexed into buildddir, pacsym assumes the length of the tarball extensions is 7 characters (example: .tar.xz and .tar.gz)
+			// The directory it was extracted to will be indexed into buildddir, pacsym assumes the length of the
+			// tarball extensions is 7 characters (example: .tar.xz and .tar.gz)
 			extensionLength := 7
 			builddir := "/usr/pkgsrc/" + tarName[:len(tarName)-extensionLength]
 
 			// This part will find the directory of the configure file, which is used for creating the makefile.
 			conflocation := builddir + "/configure"
 
-			// Now pacsym will check if the package requires for it to be built in a separate build directory, which would require --separate-builddir to be passed, or -s
+			// Now pacsym will check if the package requires for it to be built in a separate build directory,
+			// which would require --separate-builddir to be passed, or -s
 			if inList(buildarguments, "--separate-builddir") || inList(buildarguments, "-s") {
 
 				//This will create a folder called "build" in the directory the tarball created. This folder will now be the path for builddir.
@@ -262,7 +283,8 @@ func main() {
 		// When the install option is passed, it's going to discover the previously built package and put it into place, give it a name and a version.
 		case "install":
 
-			// When executing install, it requires a <PACKAGENAME> and <PACKAGEVERSION>. It will take these and index them. It will also assume everything after written are install arguments.
+			// When executing install, it requires a <PACKAGENAME> and <PACKAGEVERSION>. It will take these and index them.
+			// It will also assume everything after written are install arguments.
 			packageName := os.Args[2]
 			packageVer := os.Args[3]
 			installarguments = os.Args[4:]
@@ -312,7 +334,9 @@ func main() {
 			}
 			fmt.Println(string(stdout))
 
-			// Now the packages will be installed, Where one of the default paramaters added by pacsym is DESTDIR=/usr/pkg/packageName/packageVersion, which is the core of pacsyms functions.
+			// Now the packages will be installed, Where one of the default paramaters added by pacsym is
+			// DESTDIR=/usr/pkg/packageName/packageVersion, which is the core of pacsyms functions.
+			//
 			// It will execute make install DESTDIR=/usr/pkg/packageName/packageVersion/ and then with the parameters passed with makearguments.
 			fmt.Println("Installing...")
 			param := "DESTDIR=/usr/pkg/" + packageName + "/" + packageVer + "/"
@@ -328,10 +352,11 @@ func main() {
 			// Here pacsym will end, it's installed but it cant be used yet, it needs to be symlinked into place with pacsym sync
 			fmt.Println("Done.")
 
-		// If the clean option is passed, it will remove everything in /usr/pkgsrc, which is important as pacsym can't build a new package if the /usr/pkgsrc directory inst empty.
+		// If the clean option is passed, it will remove everything in /usr/pkgsrc
+		// which is important as pacsym can't build a new package if the /usr/pkgsrc directory isn't empty.
 		case "clean":
 
-			// Here pacsym will discover every file in /usr/pkgsrc and execute rm -r /usr/pkgsrc/sourcecode
+			// Here pacsym will discover every file in /usr/pkgsrc and execute 'rm -r /usr/pkgsrc/sourcecode'
 			files, err := ioutil.ReadDir("/usr/pkgsrc")
 			if err != nil {
 				log.Fatal(err)
